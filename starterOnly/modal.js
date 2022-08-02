7421f3131fbb34fade130b26f494e3907ff38e1f
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2022-08-02 11:06:42 +0200

    les fonctions pour les inputs de formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,12 +1,3 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -14,17 +5,120 @@ const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click",()=> launchModal()));
 
-closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+closeBtn.forEach((btn) => btn.addEventListener("click",()=> closeModal()));
 
 // launch modal form
-function launchModal() {
+const launchModal=()=> {
   modalbg.style.display = "block";
 }
 
 // Close modal form
-function closeModal() {
+const closeModal=()=> {
   modalbg.style.display = "none";
 }
 
+
+
+
+
+
+
+
+const nameChecker = (value,mesaage) => {
+  let isValid = false;
+
+  if (value.length < 2) {
+    errorDisplay.textContent = mesaage;
+  } else {
+    errorDisplay.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+};
+
+
+/**
+ * function email
+ */
+const emailChecker = (value) => {
+  let isValid = false;
+
+  if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    errorDisplay.textContent = "Veuillez entrer une adresse mail valide.";
+  } else {
+    errorDisplay.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+};
+
+/**
+ * function birthdate
+ */
+const birthdateChecker = (value) => {
+  let isValid = false;
+
+  if (!value) {
+    errorDisplay.textContent = "Veuillez entrer une date de naissance.";
+  } else {
+    errorDisplay.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+};
+
+/**
+ * function quantity
+ */
+const quantityChecker = (value) => {
+  let isValid = false;
+
+  if (!value) {
+    errorDisplay.textContent = "Veuillez entrer un chiffre.";
+  } else {
+    errorDisplay.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+};
+
+/**
+ * function checkbox */
+const checkboxContainer = () => {
+  const radios = document.querySelectorAll('input[name = "location"]');
+  isValid = false;
+
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      isValid = true;
+      errorDisplay.textContent = "";
+      break;
+    } else {
+      errorDisplay.textContent = "Veuillez sélectionner un choix.";
+      errorDisplay.style.color = "red";
+      errorDisplay.style.fontSize = "0.6em";
+    }
+  }
+  return isValid;
+};
+
+/**
+ * function checkbox cgv
+ * @returns
+ */
+const checkboxChecker = () => {
+  const check = document.querySelector(".checkbox1");
+  const checkbox1 = document.querySelector("#checkbox1");
+  let isValid = false;
+
+  if (!checkbox1.checked) {
+    errorDisplay.textContent =
+      "Vous devez vérifier que vous acceptez les termes et conditions.";
+  } else {
+    errorDisplay.textContent = "";
+    isValid = true;
+  }
+  return isValid;
+};
