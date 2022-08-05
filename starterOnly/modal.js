d8d53bc848d33b01e7ada7183967f7629aede811
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2022-08-05 13:43:49 +0200

    Fonction pour la modale de confirmation d'inscription

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -252,3 +252,25 @@ const checkboxChecker = () => {
 };
 
 submitInput.addEventListener("click", (e) => onSubmit(e));
+
+
+const btnSubmit = document.querySelector(".btn-submit");
+const modalConfirmation = document.querySelector(".formConfirmation");
+const spanValidModal = document.querySelector(".formConfirmation > span");
+const btnSubmitConfirm = document.querySelector(".btn-submit-confirm");
+ 
+
+function launchModalConfirmation() {
+  modalConfirmation.style.display = "block";
+  spanValidModal.innerHTML = "Merci pour <br> votre inscription";
+}
+
+btnSubmit.addEventListener("click", launchModalConfirmation);
+
+
+function closeModalConfirmation() {
+  modalbg.style.display = "none";
+  window.location.reload();
+}
+
+btnSubmitConfirm.addEventListener("click", closeModalConfirmation);
